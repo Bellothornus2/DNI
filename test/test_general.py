f055189afca29ae4b3727d0b29e0c7f73680b046
bellothornus2 <divanov@cifpfbmoll.eu>
2021-01-08 16:34:36 +0100

    test(DNI): :white_check_mark: we added tests to the nie field

--- test/test_general.py
+++ test/test_general.py
@@ -5,3 +5,15 @@ def test_general():
     dni.nif_searcher
     dni.nif_setter
     assert dni.nif == "12345678Z"
+
+def test_nie():
+    dni = Dni("Y1234567")
+    dni.nif_searcher
+    dni.nif_setter
+    assert dni.nif == "Y1234567X"
+
+def test_nie_x_character():
+    dni = Dni("X1234567")
+    dni.nif_searcher
+    dni.nif_setter
+    assert dni.nif == "X1234567L"
